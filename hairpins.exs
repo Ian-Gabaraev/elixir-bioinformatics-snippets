6bf4f954fbd89311b42cafbf972f9a211191d8ab
Ian Gabaraev <39344857+Ian-Gabaraev@users.noreply.github.com>
2022-02-02 01:18:12 +0300

    Add hpins module

---
+++ hairpins.exs
@@ -0,0 +1,55 @@
+require Integer
+
+defmodule HairpinSequence do
+
+  @complements %{
+    "G" => "C",
+    "C" => "G",
+    "A" => "T",
+    "T" => "A"
+  }
+
+  # Determines the length of an input hairpin sequence
+  def get_hairpin_length(nucleotide_sequence) do
+    sequence_length = String.length(nucleotide_sequence)
+    is_even_length = Integer.is_even(sequence_length)
+
+    if not are_complements(
+      String.first(nucleotide_sequence),
+      String.last(nucleotide_sequence)
+      ) do
+        0
+    end
+
+    if is_even_length do
+      if are_complements(
+        String.at(nucleotide_sequence, div(sequence_length, 2)-1),
+        String.at(nucleotide_sequence, div(sequence_length, 2))
+      ) do
+        0
+      end
+    end
+
+    indexes = Enum.to_list(0..div(sequence_length, 2)-1)
+
+    checks = Enum.map(
+      indexes, fn x -> are_complements(
+        String.at(nucleotide_sequence, x),
+        String.at(nucleotide_sequence, (sequence_length-x)-1)
+        ) end
+      )
+
+    matched_complements = Enum.filter(
+      Enum.to_list(0..length(checks)-2), fn x ->
+        Enum.at(checks, x) and Enum.at(checks, x+1) == true
+      end
+    )
+
+    length(matched_complements)+1
+
+  end
+
+  defp are_complements(base_one, base_two) do
+    @complements[base_one] == base_two
+  end
+end
