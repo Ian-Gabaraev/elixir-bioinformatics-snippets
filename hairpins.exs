1c7b6af75a52c62b32926909c315b34f29e927e2
Ian Gabaraev <39344857+Ian-Gabaraev@users.noreply.github.com>
2022-02-03 00:12:17 +0300

    fix hairpin length func

--- hairpins.exs
+++ hairpins.exs
@@ -12,24 +12,19 @@ defmodule HairpinSequence do
   # Determines the length of an input hairpin sequence
   def get_hairpin_length(nucleotide_sequence) do
     sequence_length = String.length(nucleotide_sequence)
-    is_even_length = Integer.is_even(sequence_length)
 
-    if not _are_complements(
-      String.first(nucleotide_sequence),
-      String.last(nucleotide_sequence)
-      ) do
-        0
+    cond do
+      _is_even_length_non_hp(nucleotide_sequence, sequence_length) == true ->
+        raise ArgumentError, message: "Even length non-hp"
+      _first_last_bases_non_complementary(nucleotide_sequence) == true ->
+        raise ArgumentError, message: "First and last bases are non-complementary"
+      true ->
+        _hp_len(nucleotide_sequence, sequence_length)
     end
+  end
 
-    if is_even_length do
-      if _are_complements(
-        String.at(nucleotide_sequence, div(sequence_length, 2)-1),
-        String.at(nucleotide_sequence, div(sequence_length, 2))
-      ) do
-        0
-      end
-    end
 
+  defp _hp_len(nucleotide_sequence, sequence_length) do
     indexes = Enum.to_list(0..div(sequence_length, 2)-1)
 
     checks = Enum.map(
@@ -41,15 +36,49 @@ defmodule HairpinSequence do
 
     matched_complements = Enum.filter(
       Enum.to_list(0..length(checks)-2), fn x ->
-        Enum.at(checks, x) and Enum.at(checks, x+1) == true
+        (Enum.at(checks, x) and Enum.at(checks, x+1)) == true
       end
     )
 
-    length(matched_complements)+1
+    cond do
+      length(matched_complements) == 0 ->
+        1
+      length(matched_complements) > 0 ->
+        length(matched_complements)+1
+    end
+  end
 
+
+  defp _first_last_bases_non_complementary(nucleotide_sequence) do
+    first_base = String.first(nucleotide_sequence)
+    last_base = String.last(nucleotide_sequence)
+
+    cond do
+      _are_complements(first_base, last_base) == false ->
+        true
+      _are_complements(first_base, last_base) == true ->
+        false
+    end
+  end
+
+
+  defp _is_even_length_non_hp(nucleotide_sequence, sequence_length) do
+    is_even_length = Integer.is_even(sequence_length)
+    leftmost_middle_base = String.at(nucleotide_sequence, div(sequence_length, 2)-1)
+    rightmost_middle_base = String.at(nucleotide_sequence, div(sequence_length, 2))
+
+    cond do
+      is_even_length and _are_complements(leftmost_middle_base, rightmost_middle_base) == true ->
+        true
+      is_even_length and _are_complements(leftmost_middle_base, rightmost_middle_base) == false ->
+        false
+    end
   end
 
   defp _are_complements(base_one, base_two) do
     @complements[base_one] == base_two
   end
+
 end
+
+IO.puts(HairpinSequence.get_hairpin_length("AGTATTATCT"))
